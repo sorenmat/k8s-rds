5434c4e4b9d6bf2f406f5d10c464bf590e4f6bff
Soren Mathiasen <smo@tradeshift.com>
2018-04-24 15:42:00 +0200

    Adding more docs, make backup configurable
    Describing the YAML file in more detail.
    Make the backup retention period configurable

--- crd/crd.go
+++ crd/crd.go
@@ -49,18 +49,20 @@ type Database struct {
 	Status             DatabaseStatus `json:"status,omitempty"`
 }
 
+// DatabaseSpec main structure describing the database instance
 type DatabaseSpec struct {
-	Username           string               `json:"username"`
-	Password           v1.SecretKeySelector `json:"password"`
-	DBName             string               `json:"dbname"`
-	Engine             string               `json:"engine"` // "postgres"
-	Class              string               `json:"class"`  // like "db.t2.micro"
-	Size               int64                `json:"size"`   // size in gb
-	MultiAZ            bool                 `json:"multiaz,omitempty"`
-	PubliclyAccessible bool                 `json:"publicaccess,omitempty"`
-	StorageEncrypted   bool                 `json:"encrypted,omitempty"`
-	StorageType        string               `json:"storagetype,omitempty"`
-	Iops               int64                `json:"iops,omitempty"`
+	Username              string               `json:"username"`
+	Password              v1.SecretKeySelector `json:"password"`
+	DBName                string               `json:"dbname"`
+	Engine                string               `json:"engine"` // "postgres"
+	Class                 string               `json:"class"`  // like "db.t2.micro"
+	Size                  int64                `json:"size"`   // size in gb
+	MultiAZ               bool                 `json:"multiaz,omitempty"`
+	PubliclyAccessible    bool                 `json:"publicaccess,omitempty"`
+	StorageEncrypted      bool                 `json:"encrypted,omitempty"`
+	StorageType           string               `json:"storagetype,omitempty"`
+	Iops                  int64                `json:"iops,omitempty"`
+	BackupRetentionPeriod int64                `json:"backupretentionperiod,omitempty"` // between 0 and 35, zero means disable
 }
 
 type DatabaseStatus struct {
