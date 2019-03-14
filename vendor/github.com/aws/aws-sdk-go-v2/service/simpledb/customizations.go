b01c552479e9e65e471b741b4cd304e6246ccd2b
yurifrl <yurifl03@gmail.com>
2019-03-14 15:11:31 -0300

    feautre: delete vendor

--- vendor/github.com/aws/aws-sdk-go-v2/service/simpledb/customizations.go
+++
@@ -1,9 +0,0 @@
-package simpledb
-
-func init() {
-	initClient = func(c *SimpleDB) {
-		// SimpleDB uses custom error unmarshaling logic
-		c.Handlers.UnmarshalError.Clear()
-		c.Handlers.UnmarshalError.PushBack(unmarshalError)
-	}
-}
